4a5c4ce731b6840c23bad58fbde470a84799d553
shreyas <shreyaslakhani25@gmail.com>
2025-06-17 13:08:24 -0400

    refactor: remove unused setRecommendations prop from MovieGrid component
    - Eliminate the setRecommendations prop from MovieGrid to simplify the component interface
    - Update MovieCard rendering to remove unnecessary prop passing, enhancing clarity and maintainability

--- app/components/MovieGrid.tsx
+++ app/components/MovieGrid.tsx
@@ -5,18 +5,16 @@ import MovieCard from './MovieCard'
 
 interface MovieGridProps {
   movies: Movie[]
-  setRecommendations?: React.Dispatch<React.SetStateAction<Movie[]>>
   onMovieAction?: () => void
 }
 
-export default function MovieGrid({ movies, setRecommendations, onMovieAction }: MovieGridProps) {
+export default function MovieGrid({ movies, onMovieAction }: MovieGridProps) {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 p-2 sm:p-4 md:p-6">
       {movies.map((movie) => (
         <MovieCard
           key={movie.id}
           movie={movie}
-          setRecommendations={setRecommendations || (() => {})}
           onMovieAction={onMovieAction}
         />
       ))}
